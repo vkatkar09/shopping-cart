20f90d11b289f59539a5e6a6c711a4b532d89c7d
Varsha Katkar <vkatkar09@gmail.com>
2019-12-21 18:23:33 -0800

    removed cart comp and add a modal for cart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shopping-cart';
+  products;
+  totalCartCost=0;
+  constructor(private httpClient: HttpClient) { }
+  ngOnInit() {
+    this.httpClient.get('http://localhost:3000/users/1').subscribe((res) => {
+      this.products = res["cart"]
+    });
+  }
+
+  calculateIndividualTotal(cost, quantity) {
+    return cost * quantity;
+  }
+
+  updateTotalProductCost(value, productId) {
+    this.httpClient.get('http://localhost:3000/users/1').subscribe((res) => {
+      var temp = res;
+      var tempCart = res["cart"];
+      tempCart.forEach(element => {
+        if (element.id == parseInt(productId)) {
+          element.quantity = parseInt(value)
+        }
+      });
+      temp["cart"] = tempCart
+      var tempTotal=0
+      tempCart.forEach(element => {
+        tempTotal+= parseInt(element.quantity)*parseInt(element.cost)
+       });
+       this.totalCartCost = tempTotal;
+      this.httpClient.put("http://localhost:3000/users/1", temp ).subscribe((res) => {});
+    });
+  }
+
+  updateCartTotal(){
+    this.httpClient.get('http://localhost:3000/users/1').subscribe((res) => {
+      var temp = res;
+      var tempTotal=0;
+      var tempCart = res["cart"];
+      tempCart.forEach(element => {
+       tempTotal+= parseInt(element.quantity)*parseInt(element.cost)
+      });
+      this.totalCartCost = tempTotal;
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CartComponent } from './cart/cart.component';
 import { LoginComponent } from './login/login.component';
 import { ProductsComponent } from './products/products.component';
 import { RegisterComponent } from './register/register.component';
@@ -12,7 +11,6 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    CartComponent,
     LoginComponent,
     ProductsComponent,
     RegisterComponent
